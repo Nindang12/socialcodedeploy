65c3c93b23fed1ea63bab2839e4d948c42d6e394
NemBadBoizzz <namlien39@gmail.com>
2025-05-11 17:26:51 +0700

    update get user

--- backend/DAO/DAO_manager.py
+++ backend/DAO/DAO_manager.py
@@ -20,7 +20,14 @@ class DAO_Manager:
 
     def get_user(self, user_id: str):
         return self.user_dao.get_user(user_id)
-    
+
+    # input validation and error handling (idea if usefull)
+    # def get_user(self, user_id: str):
+    #     user = self.user_dao.get_user(user_id)
+    #     if not user:
+    #         raise HTTPException(status_code=404, detail="User not found")
+    #     return user
+
     def check_user_exists(self, phone_number: str = None, email: str = None, username: str = None):
         if phone_number and self.user_dao.get_user_by_phone_number(phone_number):
             return True
